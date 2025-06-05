1aad7fd28e7f0c47cb2240ee64c2c4e889de5af3
João Marcelus <joaomarcelus6@gmail.com>
2025-06-05 09:45:11 -0300

    correção da implementação do bot de mensagem

--- paginabiopedancia/contato.js
+++ paginabiopedancia/contato.js
@@ -1,16 +1,35 @@
- document.getElementById('sendButton').addEventListener('click', function() {
-    
-        alert('Enviado');
+window.addEventListener("DOMContentLoaded", function () {
+    emailjs.init("-Tm67zutm7Lr_fz4s");
 
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('phone').value = '';
-        document.getElementById('age').value = '';
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
 
-        document.getElementById('Comentarios').value = '';
+        const serviceID = 'service_drurwbc';
+        const templateID = 'template_cht42tw';
 
-        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = false;
+        const form = this;
+        const templateParams = {
+            Nome: form.elements['Nome'].value,
+            Email: form.elements['Email'].value,
+            Telefone: form.elements['Telefone'].value,
+            Idade: form.elements['Idade'].value,
+            Comentarios: form.elements['Comentarios'].value
+        };
+
+        const tiposContatoSelecionados = [];
+        document.querySelectorAll('#contact-form input[type="checkbox"]:checked').forEach(checkbox => {
+            tiposContatoSelecionados.push(checkbox.value);
         });
+        templateParams['TipoContato'] = tiposContatoSelecionados.length > 0 ? tiposContatoSelecionados.join(', ') : 'Nenhum selecionado';
+
+        emailjs.send(serviceID, templateID, templateParams)
+            .then(() => {
+                alert('Sua mensagem foi enviada com sucesso!');
+                form.reset();
+            }, (error) => {
+                console.error('Falha ao enviar a mensagem:', error);
+                alert('Ocorreu um erro ao enviar sua mensagem. Por favor, tente novamente mais tarde.');
+                
+            });
     });
+});
