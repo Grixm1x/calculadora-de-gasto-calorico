3b3f61eb687a4b9109e125eebb8ed0601c91ec87
João Marcelus <joaomarcelus6@gmail.com>
2025-06-04 21:41:16 -0300

    implementação do contato.js

--- paginabiopedancia/pages/contato.php
+++ paginabiopedancia/pages/contato.php
@@ -19,22 +19,27 @@
             <legend><strong>Informações de contato</strong></legend>
             <input type="text" id="name" name="Nome" placeholder="Nome:"><br>
             <input type="email" id="email" name="Email" placeholder="Email:"><br>
-            <input type="number" id="number" name="Telefone" placeholder="Telefone:"><br>
-            <input type="number" id="number" name="Idade" placeholder="Idade:"><br>
-            <input type="checkbox" value="Duvida" id="Duvida" nome="pergunta1">
+            <input type="number" id="phone" name="Telefone" placeholder="Telefone:"><br>
+            <input type="number" id="age" name="Idade" placeholder="Idade:"><br>
+
+            <input type="checkbox" value="Duvida" id="Duvida" name="pergunta1">
             <label for="Duvida">Duvida</label>
-            <input type="checkbox" value="Sugestão" id="Sugestão" nome="pergunta2">
-            <label for="Sugestão">Sugestão</label>
-            <input type="checkbox" value="Experiencia" id="Experiencia" nome="pergunta3">
+            <input type="checkbox" value="Sugestão" id="Sugestao" name="pergunta2">
+            <label for="Sugestao">Sugestão</label>
+            <input type="checkbox" value="Experiencia" id="Experiencia" name="pergunta3">
             <label for="Experiencia">Experiencia</label>
-            <input type="checkbox" value="Relatarumerronosite" id="Relatar um erro no site" nome="pergunta4">
+            <input type="checkbox" value="Relatarumerronosite" id="Relatarumerronosite" name="pergunta4">
             <label for="Relatarumerronosite">Relatar um erro no site</label>
-            <input type="checkbox" value="reclamação" id="Reclamação" nome="pergunta5">
-            <label for="reclamação">Reclamação</label>
-            <textarea placeholder="Comentarios:" name="Comentarios" id="Comentarios" rows="10" cols="60"></textarea><br>
-            <button onclick="alert('Enviado')">Enviar</button><br>
+            <input type="checkbox" value="reclamacao" id="Reclamacao" name="pergunta5">
+            <label for="Reclamacao">Reclamação</label><br>
+
+            <textarea placeholder="Comentarios:" name="Comentarios" id="Comentarios" rows="10"
+                cols="60"></textarea><br><br>
+
+            <button id="sendButton">Enviar</button><br>
         </fieldset>
         <section><br>
+            <script src="contato.js"></script>
 </body>
 
 </html>
