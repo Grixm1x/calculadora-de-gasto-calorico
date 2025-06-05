e99c172691e13804c785b22d3f3dc4e80fb5e10a
João Marcelus <joaomarcelus6@gmail.com>
2025-06-05 09:46:26 -0300

    correção das ligações entre o contato.js e o chat bot

--- paginabiopedancia/pages/contato.php
+++ paginabiopedancia/pages/contato.php
@@ -6,40 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="includes/hoome.css">
     <title>Contato</title>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js"></script>
 </head>
 
 <body>
     <h1><strong>Fale Conosco</strong></h1>
-    <p>Tem alguma dúvida, sugestão ou quer compartilhar sua experiência com alguma de nossas dietas Adoramos ouvir você!
+    <p>Tem alguma dúvida, sugestão ou quer compartilhar sua experiência com alguma de nossas dietas? Adoramos ouvir
+        você!
         Entre em contato conosco através do formulário abaixo.
     </p><br>
     <section>
-        <form action="Formulario:"></form>
         <fieldset>
             <legend><strong>Informações de contato</strong></legend>
-            <input type="text" id="name" name="Nome" placeholder="Nome:"><br>
-            <input type="email" id="email" name="Email" placeholder="Email:"><br>
-            <input type="number" id="phone" name="Telefone" placeholder="Telefone:"><br>
-            <input type="number" id="age" name="Idade" placeholder="Idade:"><br>
+            <form id="contact-form">
+                <input type="text" name="Nome" placeholder="Nome:" required><br>
+                <input type="email" name="Email" placeholder="Email:" required><br>
+                <input type="number" name="Telefone" placeholder="Telefone:"><br>
+                <input type="number" name="Idade" placeholder="Idade:"><br>
 
-            <input type="checkbox" value="Duvida" id="Duvida" name="pergunta1">
-            <label for="Duvida">Duvida</label>
-            <input type="checkbox" value="Sugestão" id="Sugestao" name="pergunta2">
-            <label for="Sugestao">Sugestão</label>
-            <input type="checkbox" value="Experiencia" id="Experiencia" name="pergunta3">
-            <label for="Experiencia">Experiencia</label>
-            <input type="checkbox" value="Relatarumerronosite" id="Relatarumerronosite" name="pergunta4">
-            <label for="Relatarumerronosite">Relatar um erro no site</label>
-            <input type="checkbox" value="reclamacao" id="Reclamacao" name="pergunta5">
-            <label for="Reclamacao">Reclamação</label><br>
+                <input type="checkbox" value="Duvida" name="Duvida" id="Duvida">
+                <label for="Duvida">Dúvida</label>
 
-            <textarea placeholder="Comentarios:" name="Comentarios" id="Comentarios" rows="10"
-                cols="60"></textarea><br><br>
+                <input type="checkbox" value="Sugestao" name="Sugestao" id="Sugestao">
+                <label for="Sugestao">Sugestão</label>
 
-            <button id="sendButton">Enviar</button><br>
+                <input type="checkbox" value="Experiencia" name="Experiencia" id="Experiencia">
+                <label for="Experiencia">Experiência</label>
+
+                <input type="checkbox" value="Relatar um erro no site" name="Relatarumerronosite"
+                    id="Relatarumerronosite">
+                <label for="Relatarumerronosite">Relatar um erro no site</label>
+
+                <input type="checkbox" value="Reclamacao" name="Reclamacao" id="Reclamacao">
+                <label for="Reclamacao">Reclamação</label><br>
+
+                <textarea id="comentarios" name="Comentarios" placeholder="Comentários:" rows="10"
+                    cols="60"></textarea><br><br>
+
+                <button type="submit" id="sendButton">Enviar</button><br>
+            </form>
         </fieldset>
-        <section><br>
-            <script src="contato.js"></script>
+    </section><br>
+    <script src="contato.js"></script>
+
 </body>
 
 </html>
